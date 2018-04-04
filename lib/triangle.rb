d435c3496de37c178347fd79ec5e1d11dd03ca9e
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-04 20:08:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
  def kind 
     if self.sides.include?(0)
      raise TriangleError
-     elsif self.sides.
+     elsif self.sides.any? do |negative|
     elsif self.sides.uniq.count == 1 
      self.kind = :equilateral
      elsif self.sides.uniq.count == 2
