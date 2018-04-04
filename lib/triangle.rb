4898c829b75e683198609de0703101cf6ee6812e
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-04 19:40:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,15 +3,18 @@ class Triangle
   attr_accessor :length_one, :length_two, :length_three, :equilateral, :isosceles, :scalene, :kind
  
  def initialize(attributes)
-   @attributes.each do |key, value|
-     self.send("#{key}=", value)
-   end
+  @length_one = length_one 
+   @length_two = length_two 
+   @length_three = length_three
  end
  
  
  def kind 
-  
-  
+   if self.length_one == self.length_two && self.length_one == self.length_three
+     self.kind = :equilateral
+     elsif self.
+     self.kind = :isosceles
+   end
  end
  
 end
