b49fb63f51a6300c5d4d3560de713cdf21fc870a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-04 19:40:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,17 +6,11 @@ class Triangle
    @attributes.each do |key, value|
      self.send("#{key}=", value)
    end
-  @length_one = length_one 
-   @length_two = length_two 
-   @length_three = length_three
  end
  
  
  def kind 
-   if self.length_one == self.length_two && self.length_one == self.length_three
-     self.kind = :equilateral
-     elsif self.
-     self.kind = :isosceles
+  
    end
  end
  
