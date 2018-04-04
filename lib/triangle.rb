ac7fee96a5fb7df0960151ab08ac60d6af7c32eb
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-04 20:13:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,8 +18,10 @@ class Triangle
      raise TriangleError
      elsif self.sides.any?(&:negative?) == true
      raise TriangleError
-     elsif self.sides
-    elsif self.sides.uniq.count == 1 
+   end 
+    sorted = self.sides.sort
+   
+    if self.sides.uniq.count == 1 
      self.kind = :equilateral
      elsif self.sides.uniq.count == 2
      self.kind = :isosceles
